b7458c39ee083a928ea0ef6eea31f5d17bdb366c
Nido <Nido45228@gmail.com>
2026-06-08 00:16:05 +0800

    Add base configuration for Vite to support GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,10 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 關鍵：在這裡加上 base 設定，前後都要有斜線！
+  // 例如你的 Repo 叫 my-vue-web，這裡就寫 '/my-vue-web/'
+  base: '/vue-sfc-inflation/', 
+  
   plugins: [
     vue(),
     vueDevTools(),
